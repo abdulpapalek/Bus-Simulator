f868189ea6debd6b08b768e296099da9a7402859
abdulpapalek <alexander.amante.nec@gmail.com>
2017-12-09 04:13:23 +0000

    *WIP *added more test cases in command.json *added cardinal directions to constants.js *added shifting of position *removed constant.json

--- bus_simulator.js
+++ bus_simulator.js
@@ -1,11 +1,14 @@
 const commands = require('./command.json');
-//const constant = require('./constant.json');
+let constant = require('./constants.js');
+const PLACEBUSPATTERN = /(PLACE) [0-5],[0-5],[A-Z]+/g;
+const SPECIALCHARACTERPATTERN = /[!@#$%^&*()_+\-=\[\]{};':\\|.<>\/?]+/;
+const RETRIEVECOORDINATES = /[0-9]+/g;
+const CHECKNUMERICSPECIALCHAR = /^[a-z0-9_@./#&+-,]*$/;
 var state = {
 		xposition:null,
 		yposition:null,
-		cadinaldirection:null
+		cardinaldirection:null
 	  }
-let constant = require('./constants.js');
 function busSimulation(){
 	//console.log(commands.command);
 	var initialPositionFlag = false;
@@ -14,7 +17,7 @@ function busSimulation(){
 	commandList.forEach(function(command){
 		//check for the PLACE prefix
 		pos = command.search(constant.PLACE);
-		if(pos == 0){
+		if(pos == constant.FOUND){
 			let valid = placeBus(command);
 			if(valid == constant.FAIL){
 				return;
@@ -22,13 +25,13 @@ function busSimulation(){
 			initialPositionFlag = true;
 			return;		
 		}
-		
+
 		if(initialPositionFlag == false){			
 			return;
 		}
 		//check for numeric, special characters and lower case characters
-		pos = command.search("/^[a-z0-9_@./#&+-,]*$/");
-		if(pos > -1){
+		pos = command.search(CHECKNUMERICSPECIALCHAR);
+		if(pos > constant.NOTFOUND){
 			return;
 		}
 		switch(command) {
@@ -36,10 +39,10 @@ function busSimulation(){
 		        moveBus();
 		        break;
 		    case constant.LEFT:
-		        shiftPosition(constants.LEFT);
+		        shiftPosition(constant.LEFT);
 		        break;
 		    case constant.RIGHT:
-		        shiftPosition(constants.RIGHT);
+		        shiftPosition(constant.RIGHT);
 		        break;
 		    case constant.REPORT:
 		        reportBusPostion();
@@ -53,30 +56,93 @@ function placeBus(command){
 	if(checkPlaceValidity(command) == false){
 		return constant.FAIL;
 	}
-	var coordinates = retrievexy(command); 
+	if(findCardinalDirection(command) == constant.NOTFOUND){
+		return constant.FAIL;
+	}
+
+	var coordinates = retrievexy(command);
+	state.xposition = coordinates[0];
+	state.yposition = coordinates[1];
+	
+	return constant.SUCCESS;
 }
 function moveBus(){
-
+	switch(state.cardinaldirection){
+	    case constant.NORTH:
+	        state.xposition++;
+	        if(state.xposition > constant.MAX){
+	        	state.xposition--;
+	        }
+	        break;
+	    case constant.SOUTH:
+	        state.xposition--;
+	        if(state.xposition < constant.MIN){
+	        	state.xposition++;
+	        }
+	        break;
+	    case constant.EAST:
+	        state.yposition++;
+	        if(state.yposition > constant.MAX){
+	        	state.yposition--;
+	        }
+	        break;
+	    case constant.WEST:
+	        state.yposition--;
+	        if(state.yposition < constant.MIN){
+	        	state.yposition++;
+	        }
+	        break;
+	    default:
+	        break;
+	}
 }
 
 function reportBusPostion(){
-
+	console.log(state.xposition+","+state.yposition+","+state.cardinaldirection);
 }
 
-function shiftPosition(){
-
+function shiftPosition(position){
+	var index = constant.CARDINALDIRECTIONS.indexOf(state.cardinaldirection);
+	switch(position){
+	    case constant.RIGHT:
+	    	state.cardinaldirection = constant.CARDINALDIRECTIONS[(index + 1) % 
+	    											    constant.CARDINALDIRECTIONS.length];
+	        break;
+	    case constant.LEFT:
+	    	state.cardinaldirection = constant.CARDINALDIRECTIONS[(index + constant.CARDINALDIRECTIONS.length - 1)% 
+	    											    		  constant.CARDINALDIRECTIONS.length];
+	        break;
+	    default:
+	        break;
+	}
 }
 
 function retrievexy(command){
-	return command.match(/[0-9]+/g);
+	return command.match(RETRIEVECOORDINATES);
 }
 
 function checkPlaceValidity(command){
-	var input = specialCharacterPattern.test(command);
+	var input = SPECIALCHARACTERPATTERN.test(command);
 	if(input == false){
-		return placeBusPattern.test(command);
+		return PLACEBUSPATTERN.test(command);
 	}
 	return false;
 }
-busSimulation();
 
+function findCardinalDirection(command){
+	var pos = constant.NOTFOUND;
+	constant.CARDINALDIRECTIONS.some(function(direction) {
+		pos = command.search(direction);
+		if(pos != constant.NOTFOUND){
+			state.cadinaldirection = direction;
+			return true;
+		}
+	});
+	if(pos != constant.NOTFOUND){
+		pos = constant.FOUND;
+	}
+	return pos;
+}
+
+
+busSimulation();
--- constants.js
+++ constants.js
@@ -1,22 +1,26 @@
 'use strict';
 
-const PLACEBUSPATTERN = new RegExp("/(PLACE) [0-5],[0-5],[A-Z]+/g");
-const SPECIALCHARACTERPATTERN = new RegExp("/[!@#$%^&*()_+\-=\[\]{};':\\|.<>\/?]+/");
-
 let constants = {
-    SUCCESS: "SUCCESS",
-    FAIL   : "FAIL",
-    PLACE  : "PLACE",
-    MOVE   : "MOVE",
-    LEFT   : "LEFT",
-    RIGHT  : "RIGHT",
-    NORTH  : "NORTH",
-    SOUTH  : "SOUTH",
-    EAST   : "EAST",
-    WEST   : "WEST",
-    REPORT : "REPORT",
-    PLACEBUSPATTERN:PLACEBUSPATTERN,
-	SPECIALCHARACTERPATTERN:SPECIALCHARACTERPATTERN     
+    SUCCESS  : "SUCCESS",
+    FAIL     : "FAIL",
+    PLACE    : "PLACE",
+    MOVE     : "MOVE",
+    LEFT     : "LEFT",
+    RIGHT    : "RIGHT",
+    NORTH    : "NORTH",
+    SOUTH    : "SOUTH",
+    EAST     : "EAST",
+    WEST     : "WEST",
+    REPORT 	 : "REPORT",
+    NOTFOUND : -1,
+    FOUND    : 0,
+    MAX		 : 5,
+    MIN      : -5,
+    CARDINALDIRECTIONS : ["NORTH",
+			              "WEST",
+			              "SOUTH",
+			              "EAST"
+			             ]  
 };
 
 module.exports =
